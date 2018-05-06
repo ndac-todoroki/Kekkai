72eb923f3ea1849181eb0d982b5a4df0beb05e15
Todoroki <todoroki.auone@gmail.com>
2018-05-06 15:07:40 +0900

    prepare v0.2.2

--- rel/config.exs
+++ rel/config.exs
@@ -31,7 +31,7 @@ end
 # will be used by default
 
 release :kekkai do
-  set version: "0.2.1"
+  set version: "0.2.2"
   set applications: [
     kekkai_core: :permanent,
     kekkai_gateway: :permanent
