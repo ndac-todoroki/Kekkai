faaaff69a62662275c2dde51569f05da4a52e61c
Todoroki <todoroki.auone@gmail.com>
2018-05-06 23:17:48 +0900

    accept strings and integers

--- apps/custom_simple_schema/lib/integer_string.ex
+++ apps/custom_simple_schema/lib/integer_string.ex
@@ -1,21 +1,35 @@
 defmodule SimpleSchema.Type.IntegerString do
   @moduledoc """
-  UUID types such as `"2017-12-25"` for SimpleSchema.
+  Accepts strings which represent valid base-10 integers, or integers itself
   """
 
   @behaviour SimpleSchema
 
   @impl SimpleSchema
   def schema(_opts) do
-    :string
+    :any
   end
 
   @impl SimpleSchema
-  def from_json(_schema, value, _opts) do
+  def from_json(_schema, value, _opts) when value |> is_binary do
     case Integer.parse(value, 10) do
-      {integer, ""} -> {:ok, integer}
-      _ -> {:error, :invalid_format}
+      {integer, ""} ->
+        {:ok, integer}
+      _ ->
+        from_json_error(value)
     end
+  end
+
+  @impl SimpleSchema
+  def from_json(_schema, value, _opts) when value |> is_integer do
+    {:ok, value}
+  end
+
+  @impl SimpleSchema
+  def from_json(_schema, value, _opts), do: from_json_error(value)
+
+  defp from_json_error(value) do
+    {:error, {:invalid_argument, "value `#{value}` is not or cannot be converted into an integer."}}
   end
 
   @impl SimpleSchema
